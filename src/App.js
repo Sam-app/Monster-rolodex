39cb371d7b0068a1966342281dd8097753dad63a
Samuel <test@local.co.uk>
2019-08-06 02:36:47 +0100

    Monster card-list and search filter added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
+
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          type="search"
+          placeholder="Serach monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+        {console.log(searchField)}
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./card-list.style.css";
+import { Card } from "../card/card.component";
+
+export const CardList = props => {
+  console.log(props);
+  return (
+    <div>
+      <div className="card-list">
+        {props.monsters.map(monster => (
+          <Card key={monster.id} monster={monster} />
+        ))}
+      </div>
+    </div>
+  );
+};
